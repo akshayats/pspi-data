9a39982053af9532e1154a3dfde52fd8f62da8e0
akshayats <akshayats@gmail.com>
2014-09-24 14:26:52 +0200

    Added data files for keyboard book and beyboard+book.

---
+++ data-io-src/global.h
@@ -0,0 +1,3 @@
+extern std::string XMLDirName    = "../xml-data";
+extern std::string PCDDirName    = "/media/18F69AB9F69A969A/final-data/pcd-annotated";
+extern std::string DataDirName   = "../keyboards-groundtruth";
--- data-io-src/pcd_read.cpp
+++ data-io-src/pcd_read.cpp
@@ -10,19 +10,31 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
+// #include "global.h"
+
+// Function Declarations
+int flagIndxsOfObject(std::vector<bool> &, std::string, std::string);
+int writeOutIndxFlags(std::vector<bool> &, std::string);
+int findCombinedIndxFlags(std::vector<bool> &, std::vector<bool> &, std::vector<bool> &);
+
+// Data Directory Name
+// std::string XMLDirName    = "./xml-dir";
+// std::string PCDDirName    = "./pcd-dir";
+// std::string KeyboardsDataDirName   = "../pspi-data";
+
+std::string XMLDirName                    = "../xml-data";
+std::string PCDDirName                    = "/media/18F69AB9F69A969A/final-data/pcd-annotated";
+std::string KeyboardsDataDirName          = "../keyboards-groundtruth";
+std::string BooksDataDirName              = "../books-groundtruth";
+std::string LaptopsDataDirName            = "../laptops-groundtruth";
+std::string KeyboardsBooksDataDirName     = "../keyboards-books-groundtruth";
+
+// Main Function
 int
 main (int argc, char** argv)
 { 
   DIR *dir;
   struct dirent *ent;
-  // Data Directory Name
-  // std::string XMLDirName    = "./xml-dir";
-  // std::string PCDDirName    = "./pcd-dir";
-  // std::string DataDirName   = "../pspi-data";
-
-  std::string XMLDirName    = "../xml-data";
-  std::string PCDDirName    = "/media/18F69AB9F69A969A/final-data/pcd-annotated";
-  std::string DataDirName   = "../keyboards-groundtruth";
 
   // Open That Directory
   if ((dir = opendir (XMLDirName.c_str())) != NULL) 
@@ -35,154 +47,75 @@ main (int argc, char** argv)
 
       // Get The Filename
       std::string FileName = ent->d_name;
-
-
-
-
-                                                                      // // DEBUG
-                                                                      // if (NumOfFiles > 20)
-                                                                      //   break;
-      
-
-
       // Checking for . and .. names
       if ((FileName.compare(".") != 0) && (FileName.compare("..") != 0))
       {
         ++NumOfFiles;
-        // Make File Name For Writing Out
+// >>>>>>>>>>
+
+// <<<<<<<<<<
+        int resKeyboard, resBook, resLaptop = 99;
+        // Extract Raw File Name - No Extensions
         int dotPos            = FileName.find_last_of(".");
         std::string RawName   = FileName.substr(0, dotPos);
         // std::cout << RawName << std::endl;
+        //----------------------------------------------------
+        // KEYBOARD
+        //----------------------------------------------------
+        // Declare Object & Initialize Flag Container
+        std::string Object1("Keyboard");
+        std::vector<bool> IsIndxKeyboard;
+        // Find Out Which Points Belong To Object
+        resKeyboard   = flagIndxsOfObject(IsIndxKeyboard, Object1, RawName);
+        // Write Out Results For This File
+        std::string WriteFileName1      = KeyboardsDataDirName + "/" + RawName + "_" + Object1 +"_GroundTruth.txt";-
+        writeOutIndxFlags(IsIndxKeyboard, WriteFileName1);
 
-        // Open FileName.xml And Read In Keyboard Idices
-        // Create Read-in Filename and Write-Out Filename
-        std::string ReadFileNameXml    = XMLDirName + "/" + FileName;
-        std::string ReadFileNamePcd    = PCDDirName + "/" + RawName + ".pcd";
-        std::string WriteFileName      = DataDirName + "/" + RawName + "_" +"Keyboard"+"_GroundTruth.txt";
 
-        // Open FileName
-        std::ifstream myReadFile;
-        myReadFile.open(ReadFileNameXml.c_str());
-        // std::cerr << "Error: " << strerror(errno);
-        std::cout << FileName.c_str() << "..." << std::endl;
+        //----------------------------------------------------
+        // BOOK
+        //----------------------------------------------------
+        // Declare Object & Initialize Flag Container
+        std::string Object2("Book");
+        std::vector<bool> IsIndxBook;
+        // Find Out Which Points Belong To Object
+        resBook   = flagIndxsOfObject(IsIndxBook, Object2, RawName);
+        // Write Out Results For This File
+        std::string WriteFileName2      = BooksDataDirName + "/" + RawName + "_" + Object2 +"_GroundTruth.txt";-
+        writeOutIndxFlags(IsIndxBook, WriteFileName2);
 
-        // --------------------------------------------------
-        // Open PCD File & Load Point Cloud
-        // --------------------------------------------------
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-        if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (ReadFileNamePcd.c_str(), *cloud) == -1) //* load the file
+
+        //----------------------------------------------------
+        // LAPTOP
+        //----------------------------------------------------
+        // Declare Object & Initialize Flag Container
+        // std::string Object3("Laptop");
+        // std::vector<bool> IsIndxLaptop;
+        // // Find Out Which Points Belong To Object
+        // resLaptop   = flagIndxsOfObject(IsIndxLaptop, Object3, RawName);
+        // // Write Out Results For This File
+        // std::string WriteFileName3      = LaptopsDataDirName + "/" + RawName + "_" + Object3 +"_GroundTruth.txt";-
+        // writeOutIndxFlags(IsIndxLaptop, WriteFileName3);
+
+
+        //----------------------------------------------------
+        // KEYBOARDS BOOKS LAPTOPS COMBINED
+        //----------------------------------------------------
+        std::vector<bool> IsIndxObjects(IsIndxKeyboard.size(),0);
+        findCombinedIndxFlags(IsIndxObjects, IsIndxKeyboard, IsIndxBook);
+        // Write Out Results For This File
+        std::string WriteFileName4      = KeyboardsBooksDataDirName + "/" + RawName + "_" + Object1+"_"+Object2 +"_GroundTruth.txt";
+        writeOutIndxFlags(IsIndxObjects, WriteFileName4);
+
+        // Find Combined Flags For Indices
+        if ((resKeyboard<0) ||(resLaptop<0) ||(resBook<0))
         {
-          PCL_ERROR ("Couldn't read file %s \n", ReadFileNamePcd.c_str());
-          return (-1);
+          std::cout << FileName << "Was not processed! Moving on..."<< std:: endl;
+          continue;
         }
-        double cloudSize   = cloud->points.size();
-        // std::cout << cloudSize << std::endl;
-
-        std::cout << "Loaded "
-                  << cloud->width * cloud->height
-                  << " data points from " << ReadFileNamePcd
-                  << std::endl;
-
-        // --------------------------------------------------
-        // Open File and Start Processing For Keyboard Points
-        // --------------------------------------------------
-        if (myReadFile.is_open())
-        {
-          // Output String
-          std::string LineReadIn;
-          // Openning Write Out File
-          std::ofstream outFilePtr;
-          outFilePtr.open(WriteFileName.c_str());
-          // Initialise a Vector To Hold Indices
-          std::vector<bool> IsIndxKeyboard(cloudSize, 0);
-          int NumOfKeyboardPts   = 0;
-          int NumOfKeyboards     = 0;
-          // Set Some Flags
-          bool KeyboardFound         = false;
-          bool NextLineIndices       = false;
-          bool KeyboardFoundInFile   = false;
-          while (!myReadFile.eof()) 
-          {
-            // Get a New Line
-            getline(myReadFile, LineReadIn);
-            
-            // std::cout << KeyboardFound << "   "<< NextLineIndices << std::endl;
-            // std::cout << LineReadIn.substr(0, 200) << std::endl;
-            
-            if (KeyboardFound && NextLineIndices) // This Line Has Keyboard Points
-            {
-              std::cout << "I have found keyboard points. Processing now...\n";
-              ++NumOfKeyboards;
-
-              KeyboardFound     = false;
-              NextLineIndices   = false;
-
-              // Create a Vector To Hold Indices
-              std::vector<int> KeyboardIndxs;
-              // Remove the <indices> and </indices>
-              int startAt                  = LineReadIn.find(">");
-              int endAt                    = LineReadIn.find("</indices>");
-              std::string NewLineReadIn    = LineReadIn.substr(startAt+1, endAt-startAt-1);
-              
-              // std::cout << NewLineReadIn << std::endl;
-              
-              // Start Reading in Integers Piece By Piece To Get All Keyboard Indices
-              while(1)
-              {
-                int n;
-                int SpacePos       = NewLineReadIn.find(" ");
-                if (SpacePos < 0)
-                {
-                  break;
-                }
-                std::string iInt   = NewLineReadIn.substr(0, SpacePos);
-                n   = atoi(iInt.c_str());
-
-                // std::cout << "Found integer: " << iInt << "\n";
-                
-                // Push Integer Into Vector
-                KeyboardIndxs.push_back(n);
-                ++NumOfKeyboardPts;
-                // For Next Integer
-                NewLineReadIn.erase(0, SpacePos+1);
-              }
-              // Set All These Indices to char=1
-              for(int i = 0; i < KeyboardIndxs.size(); ++i)
-              {
-                IsIndxKeyboard.at(KeyboardIndxs.at(i))   = 1;
-              }
-              KeyboardFoundInFile   = true;
-            }
-
-            if (LineReadIn.find("Keyboard") != std::string::npos)
-            {
-              KeyboardFound   = true;
-              // std::cout << LineReadIn << std::endl;
-            }
-
-            if (KeyboardFound && (LineReadIn.find("/dimensions") != std::string::npos))
-            {
-              NextLineIndices   = true;
-            }            
-
-          } // Go To Next Line In File
-          
-          if (KeyboardFoundInFile) 
-          {
-            std::cout << "... completed processing. Found " << NumOfKeyboardPts 
-                      << " keyboard points from " << NumOfKeyboards << " number of keyboard(s)." 
-                      << std::endl;
-            // --------------------------------
-            // Write Out Results For This File
-            // --------------------------------
-            for(int it = 0; it < IsIndxKeyboard.size(); ++it)
-            {
-              outFilePtr << IsIndxKeyboard.at(it);
-            }
-          }
-          outFilePtr.close();
-        } // If File Has Openned ...
-        myReadFile.close();
+        // DEBUG
+        // if (NumOfFiles>1)
+        //   break;
       } // Valid Filenames Only
 
     } // Loop Over All Files In Directory
@@ -205,3 +138,194 @@ main (int argc, char** argv)
 
   return (0);
 }
+
+
+
+// Function Definitions
+int flagIndxsOfObject(std::vector<bool> &IsIndxObject, std::string Object, std::string RawName)
+{
+
+  // Open FileName.xml And Read In Object Idices
+  // Create Read-in Filename and Write-Out Filename
+  std::string ReadFileNameXml    = XMLDirName + "/" + RawName + ".xml";
+  std::string ReadFileNamePcd    = PCDDirName + "/" + RawName + ".pcd";
+  
+  // Open FileName
+  std::ifstream myReadFile;
+  myReadFile.open(ReadFileNameXml.c_str());
+  // std::cerr << "Error: " << strerror(errno);
+  std::cout << RawName.c_str() << "..." << std::endl;
+
+  // --------------------------------------------------
+  // Open PCD File & Load Point Cloud
+  // --------------------------------------------------
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (ReadFileNamePcd.c_str(), *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file %s \n", ReadFileNamePcd.c_str());
+    return(-1);
+  }
+  double cloudSize   = cloud->points.size();
+  // std::cout << cloudSize << std::endl;
+
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from " << ReadFileNamePcd
+            << std::endl;
+
+  // --------------------------------------------------
+  // Open File and Start Processing For Object Points
+  // --------------------------------------------------
+  if (myReadFile.is_open())
+  {
+    // Output String
+    std::string LineReadIn;
+    // Initialise a Vector To Hold Indices
+
+    IsIndxObject.assign(cloudSize, 0);
+    
+    int NumOfObjectPts   = 0;
+    int NumOfObjects     = 0;
+    // Set Some Flags
+    bool ObjectFound         = false;
+    bool NextLineIndices       = false;
+    bool ObjectFoundInFile   = false;
+    while (!myReadFile.eof()) 
+    {
+      // Get a New Line
+      getline(myReadFile, LineReadIn);
+      
+      // std::cout << ObjectFound << "   "<< NextLineIndices << std::endl;
+      // std::cout << LineReadIn.substr(0, 200) << std::endl;
+      
+      if (ObjectFound && NextLineIndices) // This Line Has Object Points
+      {
+        std::cout << "I have found "<< Object <<" points. Processing now...\n";
+        ++NumOfObjects;
+
+        ObjectFound     = false;
+        NextLineIndices   = false;
+
+        // Create a Vector To Hold Indices
+        std::vector<int> ObjectIndxs;
+        // Remove the <indices> and </indices>
+        int startAt                  = LineReadIn.find(">");
+        int endAt                    = LineReadIn.find("</indices>");
+        std::string NewLineReadIn    = LineReadIn.substr(startAt+1, endAt-startAt-1);
+        
+        // std::cout << NewLineReadIn << std::endl;
+        
+        // Start Reading in Integers Piece By Piece To Get All Object Indices
+        while(1)
+        {
+          int n;
+          int SpacePos       = NewLineReadIn.find(" ");
+          if (SpacePos < 0)
+          {
+            break;
+          }
+          std::string iInt   = NewLineReadIn.substr(0, SpacePos);
+          n   = atoi(iInt.c_str());
+
+          // std::cout << "Found integer: " << iInt << "\n";
+          
+          // Push Integer Into Vector
+          ObjectIndxs.push_back(n);
+          ++NumOfObjectPts;
+          // For Next Integer
+          NewLineReadIn.erase(0, SpacePos+1);
+        }
+        // Set All These Indices to char=1
+        for(int i = 0; i < ObjectIndxs.size(); ++i)
+        {
+          IsIndxObject.at(ObjectIndxs.at(i))   = 1;
+        }
+        ObjectFoundInFile   = true;
+      }
+
+      if (LineReadIn.find(Object) != std::string::npos)
+      {
+        ObjectFound   = true;
+        // std::cout << LineReadIn << std::endl;
+      }
+
+      if (ObjectFound && (LineReadIn.find("/dimensions") != std::string::npos))
+      {
+        NextLineIndices   = true;
+      }            
+
+    } // Go To Next Line In File
+    
+    if (ObjectFoundInFile) 
+    {
+      std::cout << "... completed processing. Found " << NumOfObjectPts 
+                << " "<< Object <<" points from " << NumOfObjects << " number of "
+                << Object <<"(s)." 
+                << std::endl;
+    }
+    myReadFile.close();
+    return(0);
+  } // If File Has Openned ...
+  else
+  {
+    PCL_ERROR ("Couldn't read file %s \n", ReadFileNameXml.c_str());
+    return(-1);
+  }
+}
+
+
+int writeOutIndxFlags(std::vector<bool> &IsIndxObject, std::string WriteFileName)
+{
+  // Openning Write Out File
+  std::ofstream outFilePtr;
+  outFilePtr.open(WriteFileName.c_str());
+  for(int it = 0; it < IsIndxObject.size(); ++it)
+  {
+    outFilePtr << IsIndxObject.at(it);
+  } 
+    outFilePtr.close();
+
+}
+
+int findCombinedIndxFlags(std::vector<bool> &AB, std::vector<bool> &A, std::vector<bool> &B)
+{
+  if (A.size() != B.size())
+        throw std::invalid_argument("differently sized bitwise operands");
+
+  std::vector<bool>::const_iterator itA = A.begin();
+  std::vector<bool>::const_iterator itB = B.begin();
+  std::vector<bool>::iterator itAB = AB.begin();
+  while (itAB < AB.end())
+      *(itAB._M_p ++) = *(itA._M_p ++) | *(itB._M_p ++); // word-at-a-time bitwise operation
+
+  return 0;
+
+}
+
+
+
+                            // std::vector<bool> A({0,1,1,0,0});
+                            // std::vector<bool> B({0,1,1,0,1});
+
+                            // for(std::vector<bool>::iterator iter = A.begin(); iter != A.end(); ++iter)
+                            // {
+                            //     std::cout << *iter << " ";
+                            // }
+                            // std::cout<< std::endl;
+
+
+                            // for(std::vector<bool>::iterator iter = B.begin(); iter != B.end(); ++iter)
+                            // {
+                            //     std::cout << *iter << " ";
+                            // }
+                            // std::cout<< std::endl;
+
+                            // std::vector<bool> AB(5,0);
+
+                            // findCombinedIndxFlags(AB, A, B);
+
+                            // for(std::vector<bool>::iterator iter = AB.begin(); iter != AB.end(); ++iter)
+                            // {
+                            //     std::cout << *iter << " ";
+                            // }
+                            // std::cout<< std::endl;
